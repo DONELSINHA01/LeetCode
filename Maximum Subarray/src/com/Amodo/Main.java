91c5648c4a8e8f9a4d30bdff44477eecfd4c08a4
Vicky <sinhadonel@gmail.com>
2022-11-30 11:27:36 +0530

    Maximum SubArray problem

---
+++ Maximum Subarray/src/com/Amodo/Main.java
@@ -0,0 +1,13 @@
+package com.Amodo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 20;
+        int b = 23;
+
+        int c = a+b;
+        System.out.println(c);
+    }
+}
